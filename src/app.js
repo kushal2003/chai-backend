bfb271208c717818ec9ca9d91c140de6877a6f4d
kushal2003 <kucoolsh@gmail.com>
2024-05-21 18:26:08 +0530

    Changes

--- src/app.js
+++ src/app.js
@@ -17,4 +17,14 @@ app.use(express.urlencoded({extended: true, limit : "16kb"}))
 
 app.use(express.static("public"))
 app.use(cookieParser)
+
+//routes import 
+import  userRouter  from "./routes/user.routes.js";
+
+
+//routes declaration , 
+app.use("/api/v1/users",userRouter) // this is a middleware
+// ab router ko laane ke liye middleware chahiye ,  pehle toh app.get use krr rhe the toh ussi mei routes aur controllers wali problem sort kr rhe hai 
+// jo url bnega woh is type ka hoga : api/v1/users/register
+
 export {app}
